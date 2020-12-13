f1d17baf5b5c9067368ea32210d869a750d26a54
owenThurm <owen.p.thurm@gmail.com>
2020-12-13 01:32:55 -0500

    Added Promo account card

--- src/Dashboard/Promo.js
+++ src/Dashboard/Promo.js
@@ -1,5 +1,29 @@
 import React from 'react';
+import { Card, Input, Switch } from 'antd';
 
-const Promo = props => (<div>promo</div>);
+class Promo extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    }
+  }
+
+  render() {
+    return(
+      <div style={{marginTop: 30, marginLeft: 40}}>
+        <Card style={{height: 340, width: 320, backgroundColor: 'rgb(36, 36, 52)',
+         borderColor: 'rgb(190, 190, 194)', borderRadius: '3vh', borderWidth: 2}}
+          title='Promo Account #1'
+          headStyle={{color: 'white', textAlign: 'center', borderWidth: 2, borderBottomColor: 'rgb(190, 190, 194)'}}>
+
+            <Input />
+            <Input />
+            <Switch />
+        </Card>
+      </div>
+    )
+  }
+}
 
 export default Promo;
