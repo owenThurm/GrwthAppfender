d2adc547bed4a6bcc38168d3d6dd263b0abd0b8d
owenThurm <owen.p.thurm@gmail.com>
2021-01-06 23:21:24 -0500

    Resized promo accounts

--- src/Dashboard/Promo.js
+++ src/Dashboard/Promo.js
@@ -13,7 +13,7 @@ class Promo extends React.Component {
   render() {
     return(
       <div style={{marginTop: 30, marginLeft: 40}}>
-        <Card style={{height: 320, width: 270, backgroundColor: 'rgb(36, 36, 52)',
+        <Card style={{height: 320, width: 250, backgroundColor: 'rgb(36, 36, 52)',
          borderColor: 'rgb(190, 190, 194)', borderRadius: '1.5vh', borderWidth: 2}}
           title='Promo Account #1'
           headStyle={{color: 'white', textAlign: 'center', borderWidth: 2, borderBottomColor: 'rgb(190, 190, 194)'}}>
