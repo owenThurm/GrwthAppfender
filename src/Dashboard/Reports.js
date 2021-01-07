66851dabb35bdabf575e0eb931ff98005151ed87
owenThurm <owen.p.thurm@gmail.com>
2021-01-06 23:29:59 -0500

    Added card for reports

--- src/Dashboard/Reports.js
+++ src/Dashboard/Reports.js
@@ -1,5 +1,10 @@
 import React from 'react';
+import { Card } from 'antd';
 
-const Reports = props => (<div>Reports</div>);
+const Reports = props => (
+    <Card bodyStyle={{color: 'white'}} style={{borderRadius: '1.5vh', height: '100vh', margin: 15, borderWidth: 2, borderColor: 'rgb(38, 41, 56)', backgroundColor: 'rgb(36, 36, 52)'}}>
+      reports
+    </Card>
+  );
 
 export default Reports;
