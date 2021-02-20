53f7b163003fb8dbb94c888821c4e7383d71ded9
owenThurm <owen.p.thurm@gmail.com>
2021-02-19 22:00:12 -0500

    Added highlighted icons

--- src/Settings/Settings.js
+++ src/Settings/Settings.js
@@ -1,42 +1,77 @@
 import React from 'react';
 import { Menu } from 'antd';
-import { Link, Switch, Route } from 'react-router-dom';
-import { SettingOutlined, CommentOutlined } from '@ant-design/icons'
+import { Link, Switch } from 'react-router-dom';
+import { SettingOutlined, HeartFilled, HeartOutlined,
+         MessageFilled, MessageOutlined, SettingFilled } from '@ant-design/icons'
 import ProtectedRoute from '../ProtectedRoute';
 import AccountSettings from './AccountSettings/AccountSettings';
 import CommentSettings from './CommentSettings/CommentSettings';
+import LikeSettings from './LikeSettings/LikeSettings';
 
-export const Settings = props => (
-  <div>
-    <Menu mode='horizontal'
-    style={{
-      marginTop: 20,
-       borderBottomColor: 'rgb(38, 41, 56)',
-        borderBottomWidth: 2}}>
-      <Menu.Item icon={<SettingOutlined />}>
-        <Link to='/settings' />
-        Account Settings
-      </Menu.Item>
+export const Settings = props => {
 
-      <Menu.Item icon={<CommentOutlined />}>
-        <Link to='/settings/comments' />
-        Comment Settings
-      </Menu.Item>
+  let path = window.location.pathname;
 
-    </Menu>
-    <Switch>
+  const likeSettingsIcon = () => {
+    console.log('path: ', path);
+    if(path == '/settings/likes') {
+      return <HeartFilled />
+    } else {
+      return <HeartOutlined />
+    }
+  }
 
+  const commentSettingsIcon = () => {
+    if(path == '/settings/comments') {
+      return <MessageFilled />
+    } else {
+      return <MessageOutlined />
+    }
+  }
 
-      <ProtectedRoute exact path='/settings/comments' component={CommentSettings}
-      props={{'userUsername': props.props.userUsername, 'usingCustomComments': props.props.usingCustomComments}}/>
+  const accountSettingsIcon = () => {
+    if(path == '/settings') {
+      return <SettingFilled />
+    } else {
+      return <SettingOutlined />
+    }
+  }
 
-      <ProtectedRoute path='/settings' component={AccountSettings}
-      props={{'userUsername': props.props.userUsername}}/>
+  return(
+    <div>
+      <Menu mode='horizontal'
+      style={{
+        marginTop: 20,
+        borderBottomColor: 'rgb(38, 41, 56)',
+          borderBottomWidth: 2}}>
+        <Menu.Item icon={accountSettingsIcon()}>
+          <Link to='/settings' />
+          Account Settings
+        </Menu.Item>
 
+        <Menu.Item icon={commentSettingsIcon()}>
+          <Link to='/settings/comments' />
+          Comment Settings
+        </Menu.Item>
 
-    </Switch>
+        <Menu.Item icon={likeSettingsIcon()}>
+          <Link to='/settings/likes'/>
+          Like Settings
+        </Menu.Item>
 
+      </Menu>
+      <Switch>
 
+        <ProtectedRoute exact path='/settings/comments' component={CommentSettings}
+        props={{'userUsername': props.props.userUsername, 'usingCustomComments': props.props.usingCustomComments}}/>
 
-  </div>
-);
+        <ProtectedRoute exact path='/settings/likes' component={LikeSettings}
+        props={{'userUsername': props.props.userUsername}}/>
+
+        <ProtectedRoute path='/settings' component={AccountSettings}
+        props={{'userUsername': props.props.userUsername}}/>
+
+      </Switch>
+    </div>
+  );
+}
